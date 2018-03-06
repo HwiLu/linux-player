64989265fa16ec70f08ba28ad20816cb8ad19b88
HwiLu <2418979865@qq.com>
2018-03-07 00:25:19 +0800

    Create add_free_ssh.sh

---
+++ system-manager/add_free_ssh.sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+#配置免密登陆集群所有节点
+
+keygen -t rsa
+
+for node in `cat nodes | grep -v ^# | awk '{print $1}'`
+
+do
+
+    echo $node
+    sshpass -p yourpassword ssh-copy-id root@$node 
+   
+done
