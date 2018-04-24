474bb51cc444973e043de21f86700a398493d500
HwiLu <2418979865@qq.com>
2018-04-24 22:35:56 +0800

    Update passwd_change_check.sh

--- system-manager/passwd_change_check.sh
+++ system-manager/passwd_change_check.sh
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 #将之前主机一致的密码改成随机产生的密码
-```vim
+
 #!/bin/sh
 #nodes means all of host you want to operate
 
@@ -11,10 +11,9 @@ do
     echo $node:$password >> password.txt
     sshpass -p 原始密码 ssh root@$node "echo root:$password | chpasswd"
 done
-```
+
 -----------------------------------------------------------------------
 #检查root密码是否正确脚本
-```vim
 #!/bin/bash
 #Guide：使用普通账号运行，运行结果输出相应的hostname代表该主机root密码正确
 for ip in `cat nodes | grep -v "#" | awk '{print $1}'`
@@ -23,4 +22,4 @@ do
        sshpass -p "Your_Passwd" ssh -o StrictHostKeyChecking=no root@$ip "hostname" | tee yanzheng.log
 
 done
-```
+
