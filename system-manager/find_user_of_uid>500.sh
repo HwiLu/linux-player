4ca0ff8cca8951c39863395750010f07e714449f
HwiLu <2418979865@qq.com>
2018-05-02 23:44:33 +0800

    Update find_user_of_uid>500.sh

--- system-manager/find_user_of_uid>500.sh
+++ system-manager/find_user_of_uid>500.sh
@@ -1,2 +1,3 @@
-awk -F':' '{if($3+0>1000) print $1}' /etc/passwd
-#$3+0把uid从字符串转换为整数
+awk -F':' '{if($3+0>500) print $1}' /etc/passwd
+#$3+0把uid从字符串转换为整数, $3 > 500 亦可
+awk -F':' '{if($3>500) print $1}' /etc/passwd
