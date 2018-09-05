6c4881a9e1999be347c6e501eb9a0750184d7c74
HwiLu <2418979865@qq.com>
2018-09-05 23:56:44 +0800

    Update sshpass批量操作示例.sh

--- system-manager/script/sshpass批量操作示例.sh
+++ system-manager/script/sshpass批量操作示例.sh
@@ -5,7 +5,7 @@ do
 	
 	sshpass -p password ssh -o StrictHostKeyChecking=no username@$ip  "command"
 	
-	scp -r root@$ip:远程目录文件 /本地目录/      #远程主机下载文件到本地 
+	scp -r root@$ip:远程目录文件 /本地目录/      #远程主机下载文件到本地 不需要远程主机与本地主机之间有信任关系
 done
 
 
