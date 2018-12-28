50b09a1b9dcee16aef28eda5e3c71a61745fecbf
HwiLu <2418979865@qq.com>
2018-12-28 14:56:41 +0800

    Update 将数据盘挂载点写入fstab.sh

--- system-manager/script/将数据盘挂载点写入fstab.sh
+++ system-manager/script/将数据盘挂载点写入fstab.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
 sed -i "/data[0-9]/d" /etc/fstab
+
 for i in `egrep "/data[0-9]" /etc/mtab |awk {'print $1'}`
 do
+
   uuid=`blkid $i |awk -F\" '{print $2}'`
+  
   data=`grep $i /etc/mtab |awk {'print $2'}`
+  
   echo "UUID=$uuid $data ext4 defaults 0 0" >> /etc/fstab
+  
 done
