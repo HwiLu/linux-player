065fa8fd7b46317a36bbcc620f2dd93c8f3e2e3c
HwiLu <2418979865@qq.com>
2019-02-25 09:12:28 +0800

    Update ping_test.sh

--- system-manager/script/ping_test.sh
+++ system-manager/script/ping_test.sh
@@ -2,9 +2,10 @@
 for ip in ` awk '{print $1} nodes'`        #nodes means the host you want to check 
 do
   ping -c 1 $ip > /dev/null 2>&1
+  
   if [ $? -eq 0 ]; then
-    echo "$ip is Online" 
+       echo "$ip is Online" 
   else
-    echo "$ip is Offline" 
+       echo "$ip is Offline" 
   fi
 done
