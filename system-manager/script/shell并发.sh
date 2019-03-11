62358700868b47fcff5fd248b6b87f62fb150cae
HwiLu <2418979865@qq.com>
2019-03-11 15:13:12 +0800

    Update shell并发.sh

--- system-manager/script/shell并发.sh
+++ system-manager/script/shell并发.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+conNum=$1 #并发数
+[ -e ./fork ] || mkfifo ./fork
+exec 3<> ./fork   
 
+rm -rf ./fork                   
+
+ 
+for i in `seq 1 5000`;
+do
+   
+    echo >&3                   
+done
+
+
+for i in `seq 1 $conNum`
+do
+    read -u3                    
+    {
+        hadoop jar xxx
+
+    }&
+done
+
+wait
+
+exec 3<&-                       
+exec 3>&-                       
