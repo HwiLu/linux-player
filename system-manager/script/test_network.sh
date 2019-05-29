71f1bcc5e89d5013afbe2b0cbab0ac772fb15c61
HwiLu <2418979865@qq.com>
2019-05-29 15:39:43 +0800

    Update test_network.sh

--- system-manager/script/test_network.sh
+++ system-manager/script/test_network.sh
@@ -6,7 +6,7 @@ list="12.28.19.47 12.28.19.50"
 
 for ip in $list
 do
-        nc -z $ip 7789 >/dev/null
+        nc -z $ip 8000 >/dev/null
         if [ $? -eq 0 ];then
                 echo "`hostname -i `  -->   $ip is ok " >> tmp_result
         else
